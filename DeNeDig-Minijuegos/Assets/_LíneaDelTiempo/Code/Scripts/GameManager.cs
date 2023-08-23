db8355edce616fe61e5418f3b87227ff21041c3b
OzcarDev <osja1601@gmail.com>
2023-08-23 11:41:06 -0600

    Modifing fichas

--- DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/GameManager.cs
+++ DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/GameManager.cs
@@ -5,11 +5,15 @@ using MyBox;
 public class GameManager : MonoBehaviour
 {
     
-    [SerializeField] private GameObject ficha;
+    [SerializeField] private GameObject fichaPrefab;
 
     public CollectionWrapper<FichaData>[] Levels;
 
-   //private List<FichaData>[] levels = new List<FichaData>[] { new List<FichaData>};
+    public int _Level;
+
+    [SerializeField] private Vector2 spawnerPosition;
+    [SerializeField] private float spaceBetween;
+    //private List<FichaData>[] levels = new List<FichaData>[] { new List<FichaData>};
 
     private void Awake()
     {
@@ -17,12 +21,36 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
-        
+        SortFichas();
     }
 
-    
+
     void Update()
     {
-        
+
     }
+    void SortFichas()
+    {
+        var fichas = Levels[_Level].Value;
+
+     
+        
+        for(int i = 0; i <= fichas.Length-1; i++)
+        {
+            var actualObject = Instantiate(fichaPrefab).GetComponent<Ficha>();  
+            
+            actualObject._FichaData = fichas[i];
+
+            actualObject.transform.position = new Vector3(spawnerPosition.x,spawnerPosition.y);
+
+            spawnerPosition.x += spaceBetween;
+        }
+    }
+
+  
+  
+
+
+
+  
 }
