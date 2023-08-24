b806ddab9d8d5343f4868976eeee8875ebc2ac00
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-08-24 07:57:28 -0600

    Agregando Drag and Drop

---
+++ DeNeDig-Minijuegos/Assets/Edgar/ScriptsE/DropSlot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropSlot : MonoBehaviour, IDropHandler
+{
+    public GameObject item;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Entro");
+
+        if (!item)
+        {
+            item = DragHandler.objBeingDraged;
+            item.transform.SetParent(transform);
+            item.transform.position = transform.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Slot"))
+        {
+            Debug.Log("SI");
+        }
+    }
+
+    private void Update()
+    {
+        if (item != null && item.transform.parent != transform)
+        {
+            Debug.Log("Remover");
+            item = null;
+        }
+    }
+}
