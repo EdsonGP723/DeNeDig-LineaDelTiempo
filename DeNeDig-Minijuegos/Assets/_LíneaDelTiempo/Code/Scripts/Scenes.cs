6c2de15b06e829e144a5f31836b8426950492112
EdsonGP723 <95706015+EdsonGP723@users.noreply.github.com>
2023-08-30 10:28:21 -0600

    Subiendo FadeIn

---
+++ DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/Scenes.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public int scene;
+    public GameObject fade;
+
+    public void StartCor(){
+        StartCoroutine(Cambio());
+    }
+   public IEnumerator Cambio(){
+        fade.SetActive(true);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(scene);
+    }
+}
