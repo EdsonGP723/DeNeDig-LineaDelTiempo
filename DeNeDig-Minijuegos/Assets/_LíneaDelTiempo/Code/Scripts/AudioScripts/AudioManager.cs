7fc87b8b9bbdc63cf0ad2502d750c32a7115c364
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-08-30 13:26:04 -0600

    musicmanager

---
+++ DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/AudioScripts/AudioManager.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using System.Collections;
+public class AudioManager : MonoBehaviour
+{
+	public static AudioManager Instance;
+	public enum AudioChannel { Master, Music, Ambient, fx };
+
+	[Range(0, 1)]
+	public float masterVolume = 1; // Volumen de todo
+	[Range(0, 1)]
+	public float musicVolume = 1f; // volumen de musica
+	[Range(0, 1)]
+	public float fxVolume = 1; // volumen de efectos de sonido
+
+	public bool MusicIsLooping = true;
+	
+
+	
+	AudioSource musicSource;
+	
+	AudioSource fxSource;
+
+	
+	// Aqui van todos los sonidos y musica en las librerias
+	SoundLibrary soundLibrary;
+	MusicLibrary musicLibrary;
+	
+
+	
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		
+		soundLibrary = GetComponent<SoundLibrary>();
+		musicLibrary = GetComponent<MusicLibrary>();
+		
+		// Creando los audio sources
+		
+		GameObject newfxSource = new GameObject("2D fx source");
+		fxSource = newfxSource.AddComponent<AudioSource>();
+		newfxSource.transform.parent = transform;
+		fxSource.playOnAwake = false;
+
+		GameObject newMusicSource = new GameObject("Music source");
+		musicSource = newMusicSource.AddComponent<AudioSource>();
+		newMusicSource.transform.parent = transform;
+		musicSource.loop = MusicIsLooping; // Musica se esta loopeando
+		musicSource.playOnAwake = false;
+
+		
+		SetVolume(masterVolume, AudioChannel.Master);
+		SetVolume(fxVolume, AudioChannel.fx);
+		SetVolume(musicVolume, AudioChannel.Music);
+	}
+
+	//volumen de todos los elementos
+	public void SetVolume(float volumePercent, AudioChannel channel)
+	{
+		switch (channel)
+		{
+			case AudioChannel.Master:
+				masterVolume = volumePercent;
+				break;
+			case AudioChannel.fx:
+				fxVolume = volumePercent;
+				break;
+			case AudioChannel.Music:
+				musicVolume = volumePercent;
+				break;
+			
+		}
+
+		// volumen del audio source
+		fxSource.volume = fxVolume * masterVolume;
+		musicSource.volume = musicVolume * masterVolume;
+	}
+	public void PlayMusic(string musicName, float delay)
+	{
+		musicSource.clip = musicLibrary.GetClipFromName(musicName);
+		musicSource.PlayDelayed(delay);
+	}
+
+	//Parar musica
+	public void StopMusic()
+	{
+		musicSource.Stop();
+	}
+
+
+	public void PlaySound2D(string soundName)
+	{
+		fxSource.PlayOneShot(soundLibrary.GetClipFromName(soundName), fxVolume * masterVolume);
+	}
+
+	
+}
