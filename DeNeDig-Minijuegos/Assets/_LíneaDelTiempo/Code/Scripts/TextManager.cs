f4e9b63b14342ae79eab0cbd6fab114968686a99
OzcarDev <osja1601@gmail.com>
2023-08-31 10:58:08 -0600

    Update TextManager.cs

--- DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/TextManager.cs
+++ DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/TextManager.cs
@@ -43,6 +43,7 @@ public class TextManager : MonoBehaviour
                 
                 result = data[i + PlayerPrefs.GetInt("Idioma")+1];
                 
+
             }
            
         }
