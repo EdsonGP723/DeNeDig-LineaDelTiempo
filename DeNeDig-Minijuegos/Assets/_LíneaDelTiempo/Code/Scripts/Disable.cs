6f18a0bebbe3eab91784663367341ca382f4b55e
EdsonGP723 <95706015+EdsonGP723@users.noreply.github.com>
2023-09-01 10:43:30 -0600

    Terminando cuenta regresiva

---
+++ DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/Disable.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Disable : MonoBehaviour
+{
+    public float time;
+    public float limit;
+    public bool _timmerIsRunning = false;
+    public TextMeshProUGUI contador;
+    public void Start(){
+        
+        _timmerIsRunning = true;
+        StartCoroutine(Off());
+    }
+
+    void Update()
+    {
+        
+        if (_timmerIsRunning == true){
+        if (time > limit)
+        {
+            time -= Time.deltaTime;
+            Display(time);
+        }
+        else {
+            _timmerIsRunning = false;
+            time = limit;
+        }
+        }
+        
+    }
+
+    private void Display(float timeToDisplay){
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(time/60);
+        float seconds = Mathf.FloorToInt(time%60);
+        contador.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public IEnumerator Off(){
+        yield return new WaitForSeconds(3);
+        this.gameObject.SetActive(false);
+    }
+}
