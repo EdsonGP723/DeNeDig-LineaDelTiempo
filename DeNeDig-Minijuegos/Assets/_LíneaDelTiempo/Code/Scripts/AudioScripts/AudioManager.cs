1b769b2a95fa77c3ac469ef859d23b3b9fe70939
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-09-12 10:13:26 -0600

    Fade en musica y musica ambiental

--- DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/AudioScripts/AudioManager.cs
+++ DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/AudioScripts/AudioManager.cs
@@ -6,47 +6,41 @@ public class AudioManager : MonoBehaviour
 	public enum AudioChannel { Master, Music, Ambient, fx };
 
 	[Range(0, 1)]
-	public float masterVolume = 1; // Volumen de todo
+	public float masterVolume = 1; // Overall volume
 	[Range(0, 1)]
-	public float musicVolume = 1f; // volumen de musica
+	public float musicVolume = 1f; // Music volume
 	[Range(0, 1)]
-	public float fxVolume = 1; // volumen de efectos de sonido
+	public float ambientVolume = 1; // Ambient volume
+	[Range(0, 1)]
+	public float fxVolume = 1; // FX volume
 
 	public bool MusicIsLooping = true;
-	
+	public bool AmbientIsLooping = true;
+	public bool CoroutineRun; 
 
 	
 	AudioSource musicSource;
-	
+	AudioSource ambientSource;
 	AudioSource fxSource;
 
-	AudioSource narracionSource;
-
 	
-	// Aqui van todos los sonidos y musica en las librerias
 	SoundLibrary soundLibrary;
 	MusicLibrary musicLibrary;
-	NarracionLibrary narracionLibrary;
-	
+	AmbientLibrary ambientLibrary;
 
 	
 	private void Awake()
 	{
-		if (Instance == null)
-		{
-			Instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
-		else if (Instance != this)
-		{
-			Destroy(gameObject);
-		}
+		if (Instance == null) Instance = this;
+		else if (Instance != this) Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject); 
 
 		
 		soundLibrary = GetComponent<SoundLibrary>();
 		musicLibrary = GetComponent<MusicLibrary>();
-		
-		// Creando los audio sources
+		ambientLibrary = GetComponent<AmbientLibrary>();
+
 		
 		GameObject newfxSource = new GameObject("2D fx source");
 		fxSource = newfxSource.AddComponent<AudioSource>();
@@ -56,19 +50,23 @@ public class AudioManager : MonoBehaviour
 		GameObject newMusicSource = new GameObject("Music source");
 		musicSource = newMusicSource.AddComponent<AudioSource>();
 		newMusicSource.transform.parent = transform;
-		musicSource.loop = MusicIsLooping; // Musica se esta loopeando
+		musicSource.loop = MusicIsLooping; 
 		musicSource.playOnAwake = false;
 
-		///////////////////////////////////
+		GameObject newAmbientsource = new GameObject("Ambient source");
+		ambientSource = newAmbientsource.AddComponent<AudioSource>();
+		newAmbientsource.transform.parent = transform;
+		ambientSource.loop = AmbientIsLooping; 
+		ambientSource.playOnAwake = false;
 
 		
-		/*SetVolume(masterVolume, AudioChannel.Master);
+		SetVolume(masterVolume, AudioChannel.Master);
 		SetVolume(fxVolume, AudioChannel.fx);
-		SetVolume(musicVolume, AudioChannel.Music);*/
-
+		SetVolume(musicVolume, AudioChannel.Music);
+		SetVolume(ambientVolume, AudioChannel.Ambient);
 	}
 
-	//volumen de todos los elementos
+	
 	public void SetVolume(float volumePercent, AudioChannel channel)
 	{
 		switch (channel)
@@ -82,38 +80,113 @@ public class AudioManager : MonoBehaviour
 			case AudioChannel.Music:
 				musicVolume = volumePercent;
 				break;
-			
+			case AudioChannel.Ambient:
+				ambientVolume = volumePercent;
+				break;
 		}
 
-		// volumen del audio source
+		
 		fxSource.volume = fxVolume * masterVolume;
 		musicSource.volume = musicVolume * masterVolume;
+		ambientSource.volume = ambientVolume * masterVolume;
 	}
+
+	
+	// Cunado delay es 0 = No delay
+	
 	public void PlayMusic(string musicName, float delay)
 	{
 		musicSource.clip = musicLibrary.GetClipFromName(musicName);
 		musicSource.PlayDelayed(delay);
-		SetVolume(musicVolume, AudioChannel.Music);
 	}
 
-	//Parar musica
+	
+	// fade
+	
+	public IEnumerator PlayMusicFade(string musicName, float duration)
+	{
+		CoroutineRun = true; 
+
+		float startVolume = 0;
+		float targetVolume = musicSource.volume;
+		float currentTime = 0;
+
+		musicSource.clip = musicLibrary.GetClipFromName(musicName);
+		musicSource.Play();
+
+		while (currentTime < duration)
+		{
+			currentTime += Time.deltaTime;
+			musicSource.volume = Mathf.Lerp(startVolume, targetVolume, currentTime / duration);
+			yield return null;
+		}
+
+		CoroutineRun = false; 
+
+		yield break;
+	}
+
+	
+	// Stop music
+	
 	public void StopMusic()
 	{
 		musicSource.Stop();
 	}
 
+	
+	// Stop music fade out
+	
+	public IEnumerator StopMusicFade(float duration)
+	{
+		CoroutineRun = true; 
 
+		float currentVolume = musicSource.volume;
+		float startVolume = musicSource.volume;
+		float targetVolume = 0;
+		float currentTime = 0;
+
+		while (currentTime < duration)
+		{
+			currentTime += Time.deltaTime;
+			musicSource.volume = Mathf.Lerp(startVolume, targetVolume, currentTime / duration);
+			yield return null;
+		}
+		musicSource.Stop();
+		musicSource.volume = currentVolume;
+
+		CoroutineRun = false; 
+
+		yield break;
+	}
+
+	
+	// Play ambient sound with delay 0 = No delay
+	
+	public void PlayAmbient(string ambientName, float delay)
+	{
+		ambientSource.clip = ambientLibrary.GetClipFromName(ambientName);
+		ambientSource.PlayDelayed(delay);
+	}
+
+	
+	// Stop ambient sound
+	
+	public void StopAmbient()
+	{
+		ambientSource.Stop();
+	}
+
+	
+	// FX Audio
+	
 	public void PlaySound2D(string soundName)
 	{
 		fxSource.PlayOneShot(soundLibrary.GetClipFromName(soundName), fxVolume * masterVolume);
 	}
 
-	public void PlayNarracion(int name)
-    {
-		narracionSource.PlayOneShot(narracionLibrary.GetNarracionFromName(name));
-		
-    }
-
-
-	
+	public void PlaySound3D(string soundName, Vector3 soundPosition)
+	{
+		AudioSource.PlayClipAtPoint(soundLibrary.GetClipFromName(soundName), soundPosition, fxVolume * masterVolume);
+	}
 }
