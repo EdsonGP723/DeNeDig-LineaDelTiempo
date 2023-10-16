cd29055e1dc59f9e38beaa6b505300a78e10a13a
OzcarDev <osja1601@gmail.com>
2023-10-16 10:24:02 -0600

    LeaderBoard

--- DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/MenuManager.cs
+++ DeNeDig-Minijuegos/Assets/_LíneaDelTiempo/Code/Scripts/MenuManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Dan.Main;
 
 public class MenuManager : MonoBehaviour
 {
@@ -17,9 +18,13 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject menuPanel;
     [SerializeField] private GameObject reglasPanel;
 
+    private static string publicKey = "4d20065bde55aba7263e07336e9700cc9ed70e2784ecc740ae4253ae301e2e5a";
 
+    [SerializeField] private List<TextMeshProUGUI> userNames;
+    [SerializeField] private List<TextMeshProUGUI> scores;
     private void Start()
     {
+        GetLeaderBoard();
         if (Globals.idiomaWasSelected){
             idiomaScreen.SetActive(false);
             menuScreen.SetActive(true);
@@ -30,6 +35,7 @@ public class MenuManager : MonoBehaviour
     public void idiomaSelected()
     {
         Globals.idiomaWasSelected = true;
+        
     }
 
     public void Play()
@@ -54,5 +60,17 @@ public class MenuManager : MonoBehaviour
     {
         PlayerPrefs.SetInt("Idioma", language);
         Debug.Log(PlayerPrefs.GetInt("Idioma"));
+    }
+
+    public void GetLeaderBoard()
+    {
+
+        LeaderboardCreator.GetLeaderboard(publicKey, ((msg) => {
+            for (int i = 0; i < userNames.Count; ++i)
+            {
+                userNames[i].text = (msg[i].Rank + ".-" + msg[i].Username);
+                scores[i].text = msg[i].Score.ToString();
+            }
+        }));
     }
 }
